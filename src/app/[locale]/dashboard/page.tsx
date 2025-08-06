2fdf7b367eb69a678c10d43cfabe44555f3024e8
MagedNour <magednour1995@gmail.com>
2025-08-07 02:54:09 +0300

    fix(deployment): eslint warning resolved

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,7 +1,4 @@
-import { getAllStatistics } from "@/lib/apis/dashboard/all-statistics.api";
-
 export default async function Overview() {
-  const Statistics = await getAllStatistics();
   return (
     <main className="bg-gray-100 w-full h-screen ">
       <h1>Overview</h1>
