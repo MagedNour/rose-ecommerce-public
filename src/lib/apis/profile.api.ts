cda3c5bbc2d3df5ccb7d7e86a925a4aa0c504954
MagedNour <magednour1995@gmail.com>
2025-08-13 05:25:57 +0300

    fix(profile) fixed JWT Token for the fetchUserData function

--- src/lib/apis/profile.api.ts
+++ src/lib/apis/profile.api.ts
@@ -1,13 +1,35 @@
-import { AUTH_COOKIE } from "@/lib/constants/auth.constant";
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
+export const AUTH_COOKIE = (() => {
+  const secureCookie = process.env.NEXTAUTH_URL?.startsWith("https://");
+  if (secureCookie) {
+    return "__Secure-next-auth.session-token";
+  } else {
+    return "next-auth.session-token";
+  }
+})();
+
 export async function fetchUserData() {
   const tokenCookies = cookies().get(AUTH_COOKIE)?.value;
+
+  // // If no cookie, user is not authenticated
+  if (!tokenCookies) {
+    throw new Error("Authentication cookie not found");
+  }
+
+  //Decode the JWT using the NextAuth secret
   const token = await decode({ token: tokenCookies, secret: process.env.NEXTAUTH_SECRET! });
 
+  // If decoding failed or token field missing → invalid session
+  if (!token?.token) {
+    throw new Error("Invalid authentication token");
+  }
+
+  // API endpoint
   const apiUrl = `${process.env.API}/auth/profile-data`;
 
+  // 4️⃣ Fetch the user profile
   const response = await fetch(apiUrl, {
     method: "GET",
     credentials: "include",
